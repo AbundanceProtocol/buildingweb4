0aeeb39c8a98a7003b9574e678bf950a21c9e265
Mike Natanzon <50894218+buildingweb4@users.noreply.github.com>
2021-12-09 04:25:11 -0500

    Added page router

---
+++ Website/src/Pages/Roadmap.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../index.css'
+
+const Roadmap = () => {
+    return (
+        <div className="col-fff">
+            <h1>Roadmap</h1>
+        </div>
+    );
+};
+
+export default Roadmap;
---
+++ Website/src/Pages/Topnav.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../bw4-logo.svg';
+import { FaGithub, FaSearch, FaPencilAlt, FaUserAlt, FaListUl, FaComments, FaYoutube, FaTwitter, FaDiscord, FaMediumM } from 'react-icons/fa';
+import { ImFileText2 } from 'react-icons/im';
+import { AiOutlineHome } from 'react-icons/ai';
+import { VscTasklist, VscKey } from 'react-icons/vsc';
+import { RiFileList3Line } from 'react-icons/ri';
+import { MdAlternateEmail } from 'react-icons/md';
+import { GiGearHammer } from 'react-icons/gi';
+import { BsQuestionCircle, BsMap } from 'react-icons/bs';
+import '../index.css';
+
+
+const Topnav = () => {
+  return (
+    <div className="flex-row flex-middle">
+      <NavLogo />
+      {/* <div className="flex-gr"></div> */}
+      <Link to="/" className="no-under">
+          <HeaderIcon iconType="home" />
+      </Link>
+      <Link to="/roadmap" className="no-under">
+          <HeaderIcon iconType="roadmap" />
+      </Link>
+      <Link to="/how-it-works" className="no-under">
+          <HeaderIcon iconType="how it works" />
+      </Link>
+      <Link to="/white-paper" className="no-under">
+          <HeaderIcon iconType="whitepaper" />
+      </Link>
+      <Link to="/faq" className="no-under">
+          <HeaderIcon iconType="faq" />
+      </Link>
+      <Link to="/connect" className="no-under">
+          <HeaderIcon iconType="connect" />
+      </Link>
+    </div>
+  );
+}
+
+const NavLogo = () => {
+  return (
+    <div className="flex-col flex-middle">
+      <div className="size-87 flex-row flex-middle">
+        <img src={Logo} className="size-50" alt="logo" />
+        <div className="flex-col col-ccc font-20" style={{margin: '0 0 0 10px'}}>
+          <div>Building Web 4.0</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const icons = {
+  'home': AiOutlineHome, 'roadmap': BsMap, 'how it works': GiGearHammer, 'whitepaper': RiFileList3Line, 'faq': BsQuestionCircle, 'connect': VscKey, 'search': FaSearch, 'github': FaGithub, 'youtube': FaYoutube, 'twitter': FaTwitter, 'discord': FaDiscord, 'medium': FaMediumM, 'post': ImFileText2, 'create': FaPencilAlt, 'user': FaUserAlt, 'email': MdAlternateEmail, 'comments': FaComments, 'summary': VscTasklist, 'categories': FaListUl
+};
+
+function Icon(props) {
+  const HeaderIcon = icons[props.iconType];
+  return <HeaderIcon className="size-25 col-ccc" />
+}
+
+const HeaderIcon = (props) => {
+  return (
+    <div className="size-87 flex-col flex-middle">
+      <div className="flex-col flex-middle">
+        <Icon iconType={props.iconType} className="size-25 col-ccc" />
+        <div className="col-ccc font-15 mar-t-6">{props.iconType}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Topnav;
